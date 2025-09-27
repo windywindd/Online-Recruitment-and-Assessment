5f3178b388a770dd604b5030984e40b1e76c9cf7
chhengheang <chhengheangmeng@gmail.com>
2025-09-27 13:52:14 +1000

    setup new repo

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://3.107.86.184:5001', // live
+  baseURL: 'http://localhost:5001', // local
+  //baseURL: 'http://3.107.86.184:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 axiosInstance.interceptors.request.use(
